2edff82ed82ae94f42700a410c7f2e232f84e421
Scott J. Roberts <sroberts@github.com>
2014-07-26 12:32:50 -0400

    added consistent regex options

--- jager.py
+++ jager.py
@@ -44,17 +44,16 @@ logger.critical('critical message')
 '''
 
 # Indicators
-re_ipv4 = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
-re_email = re.compile("\\b[A-Za-z0-9_.]+@[0-9a-z.-]+\\b", re.I)
+re_ipv4 = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", re.I | re.S | re.M)
+re_email = re.compile("\\b[A-Za-z0-9_.]+@[0-9a-z.-]+\\b", re.I | re.S | re.M)
 re_domain = re.compile("([a-z0-9-_]+\\.){1,4}(com|aero|am|asia|au|az|biz|br|ca|cat|cc|ch|co|coop|cx|de|edu|fr|gov|hk|info|int|ir|jobs|jp|kr|kz|me|mil|mobi|museum|name|net|nl|nr|org|post|pre|ru|tel|tk|travel|tw|ua|uk|uz|ws|xxx)", re.I | re.S | re.M)
 re_cve = re.compile("(CVE-(19|20)\\d{2}-\\d{4})", re.I | re.S | re.M)
 
 # Hashes
-re_md5 = re.compile("\\b[a-f0-9]{32}\\b", re.I)
-re_sha1 = re.compile("\\b[a-f0-9]{40}\\b", re.I)
-re_sha256 = re.compile("\\b[a-f0-9]{64}\\b", re.I)
-re_sha512 = re.compile("\\b[a-f0-9]{128}\\b", re.I)
-re_ssdeep = re.compile("\\b[0-9]+:[A-Za-z0-9+/]+:[A-Za-z0-9+/]+\\b", re.I)
+re_md5 = re.compile("\\b[a-f0-9]{32}\\b", re.I | re.S | re.M)
+re_sha1 = re.compile("\\b[a-f0-9]{40}\\b", re.I | re.S | re.M)
+re_sha256 = re.compile("\\b[a-f0-9]{64}\\b", re.I | re.S | re.M)
+re_sha512 = re.compile("\\b[a-f0-9]{128}\\b", re.I | re.S | re.M)
 re_ssdeep = re.compile("\\b\\d{2}:[A-Za-z0-9/+]{3,}:[A-Za-z0-9/+]{3,}\\b", re.I | re.S | re.M)
 
 # File Types
