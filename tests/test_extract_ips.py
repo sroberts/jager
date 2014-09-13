215ddac2cd6e0ecbef31ed56720032dc6c90d6b8
Kyle Maxwell <kylem@xwell.org>
2014-09-13 11:11:01 -0500

    Basic test for IPv4 addresses

---
+++ tests/test_extract_ips.py
@@ -0,0 +1,15 @@
+import nose
+
+from jager import *
+
+def test_extract_ips():
+    good_ips = ['192.168.1.1',
+                '192.168.001.001',
+                '1.1.1.1',
+                '254.254.254.254']
+    bad_ips  = ['1.1.1',
+                '1256.1256.1256.1256']
+    for ip in good_ips:
+        assert(extract_ips(ip)['ipv4addresses'][0] == ip)
+    for ip in bad_ips:
+        assert(extract_ips(ip)['ipv4addresses'] == [])
