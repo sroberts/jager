b2d05febbcaa836bba9aa42b85203b69efe015c3
Scott Roberts <sroberts@github.com>
2015-02-16 22:46:32 -0500

    turns out setup.py is needed

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+from jager import __version__
+
+setup(
+    name="jager",
+    version=__version__,
+    author="Scott J Roberts & Kyle Maxwell",
+    author_email="opensource@yolothreat.com",
+    description="Entity extraction for common threat documents.",
+    packages=[],
+)
