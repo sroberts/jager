01627fa33b94742b7e32ae9b9acb94901ff121a0
Scott J Roberts <sroberts@github.com>
2015-03-21 14:44:47 -0400

    first commit for www

---
+++ parsers/www.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+www.py
+
+Created by Scott Roberts.
+Copyright (c) 2013 TogaFoamParty Studios. All rights reserved.
+
+A module for Jager to extract plain text from websites.
+"""
+
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class JagerWWW:
+
+    # regex = "" # WIP: Check to ensure submitted value is a http link
+    text = ""
+
+    def __init__(self, website):
+        self.text = self.extractor(website)
+
+    def extractor(self, website):
+        html = requests.get(website, verify=False)
+        soup = BeautifulSoup(html.text, "html.parser")
+
+        return soup.get_text()
+
+    def __str__(self):
+
+        try:
+            return str(self.text)
+        except UnicodeEncodeError:
+            print "Unicode is a pain..."
+            raise
+
+
+def main():
+    print "WWW Text Extraction: {}".format(sys.argv[1])
+    p = JagerWWW(sys.argv[1])
+
+    print str(p)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print "User aborted."
+    except SystemExit:
+        pass
