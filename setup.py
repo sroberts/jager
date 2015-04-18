71f3cf8a1cc90bdd8ab7be254da5924aa9178011
Kyle Maxwell <krmaxwell@gmail.com>
2015-04-18 20:44:08 +0000

    Squashed 'utilitybelt/' changes from 47f1031..b4802fc
    b4802fc Fix tests for reverse_dns_sna() to call the right function
    838b909 Add additional tests for #29
    1af6d91 Fix comment and also test pre-commit for this file
    bf6093e Install pre-commit
    0f04343 Add badges as listed in #27. It's what the cool kids do.
    e7f751e Track code coverage with Coveralls.io
    5c33839 Calculate code coverage for tests
    9d73272 Linter cleanup
    48bf7d2 Use distutils instead of setuptools and specify package
    3f3feea Add package_dir parameter to setup
    1e9ee0b Move requirements into setup.py and fix requirements conflict
    37b8302 Add pytest requirement and specify version conditions
    5fd4da3 Use region_code instead of region property
    4d095a7 Fix overrides to be a list
    76ef4f3 Fix pygeoip call and set up CircleCI more correcter
    6626654 Update to newest
    343cf52 Replace GeoIP with pygeoip
    
    git-subtree-dir: utilitybelt
    git-subtree-split: b4802fc8868a4045624a3753390783b8a468507f

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup, find_packages
-
+from distutils.core import setup
 
 setup(
     name="cnd-utilitybelt",
@@ -7,7 +6,14 @@ setup(
     description="Utilities to make you a CND Batman",
     url="https://github.com/yolothreat/utilitybelt",
     license="MIT",
-    packages=find_packages(),
     include_package_data=True,
-    install_requires=['requests', 'GeoIP']
+    install_requires=['requests>=2.6.0',
+                      'pygeoip==0.3.2',
+                      'pytest>=2.6.0',
+                      'pytest-cov>=1.8.1',
+                      'coveralls>=0.5',
+                      'pre-commit>=0.4.4'
+                      ],
+    package_dir={'utilitybelt': 'src'},
+    packages=['utilitybelt']
 )
